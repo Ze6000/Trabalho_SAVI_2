734e9e6a3856cea9daca0fc47b4f4447a8521656
Carolf27 <carolinaf@ua.pt>
2024-02-06 15:47:30 +0000

    resolve audio

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from objects import Object
 # Import the required module for text  
 # to speech conversion 
 from gtts import gTTS 
+
   
 # This module is imported so that we can  
 # play the converted audio 
@@ -75,18 +76,17 @@ def main():
     # Language in which you want to convert 
     language = 'en'
     
-    # Passing the text and language to the engine,  
-    # here we have marked slow=False. Which tells  
-    # the module that the converted audio should  
     # have a high speed 
     myobj = gTTS(text=speech, lang=language, slow=False) 
     
     # Saving the converted audio in a mp3 file named 
     # welcome  
-    myobj.save("welcome.mp3") 
-    
-    # Playing the converted file 
-    os.system("welcome.mp3") 
+    myobj.save('description.mp3') 
+
+    # Specify the full path to the MP3 file
+    speech_file = 'description.mp3'
+    os.system('ffplay -v 0 -nodisp -autoexit ' + speech_file)
+
 
 
 
